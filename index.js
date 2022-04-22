eb61d94d08537b2b7e673164a391a7079a5ad1cf
Hassnain Ali <danishkhanxd@gmail.com>
2022-04-22 05:52:44 +0300

    UIadded

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 //create a list of possible choices
 let computerScore = 0
 let userScore = 0
+let count = 0
+let text = ''
 
 // make a function for computer to select a random choice from the given three
 function computerSelection() {
@@ -11,78 +13,106 @@ function computerSelection() {
     return options[Math.floor(Math.random() * options.length)]
 }
 
+function startGame() {
+  //starts the game
+}
+
 // Make a function that plays a round of game and log out the winner. takes parameters computerChoice() and userChoice.
 
 function playRound(userChoice) {
+  count++
   let computerChoice = computerSelection()
+
   if(computerChoice === "paper" && userChoice === "rock") {
     computerScore += 1
-    console.log("You Lose! Paper beats rock..")
+    text += "<br>You Lose! Paper beats rock.."
   } 
   else if(computerChoice === "rock" && userChoice === "sisor") {
     computerScore += 1
-    console.log("You Lose! Rock beats sisor...")
+    text += "<br>You Lose! Rock beats sisor..."
   } 
   else if(computerChoice === "sisor" && userChoice === "paper") {
     computerScore += 1
-    console.log("You Lose! Sisor beats paper...")
+    text += "<br>You Lose! Sisor beats paper..."
   }
     
   else if(computerChoice === "rock" && userChoice === "paper") {
     userScore += 1
-    console.log("You Win! Paper beats rock...")
+    text += "<br>You Win! Paper beats rock..."
   }
   else if(computerChoice === "sisor" && userChoice === "rock") {
     userScore += 1
-    console.log("You Win! Rock beats sisor...")
+    text += "<br>You Win! Rock beats sisor..."
   }
   else if(computerChoice === "paper" && userChoice === "sisor") {
     userScore += 1
-    console.log("You Win! Sisor beats paper...")
+    text += "<br>You Win! Sisor beats paper..."
   }
   else if(computerChoice === "paper" && userChoice === "paper") {
-    console.log("This Round is a Draw...")
+    text += "<br>This Round is a Draw..."
   }
   else if(computerChoice === "sisor" && userChoice === "sisor") {
-    console.log("This Round is a Draw...")
+    text += "<br>This Round is a Draw..."
   }
   else if(computerChoice === "rock" && userChoice === "rock") {
-    console.log("This Round is a Draw...")
+    text += "<br>This Round is a Draw..."
   }
   else {
-    console.log("Invalid Input! Please check you spellings...")
+    text += "<br>Invalid Input! Please check you spellings...";
+  }
+
+  let cR = document.getElementById("currentRound")
+  cR.innerHTML = text
+  getScore()
+}
+
+function getScore(){
+  let cS = document.getElementById("computerS")
+  let uS = document.getElementById("userS")
+  cS.innerHTML = "<p>Computer Score " + computerScore + " Points!</p>"
+  uS.innerHTML = "<p>User Score " + userScore + " Points!</p>"
+
+  if(count == 5) {
+    winner()
   }
 }
 
-// make a function that repeat the playRound function for 5 times and displays the winner at the end
-
-function Game() {
-  for(let i = 0; i < 5; i++) {
-    playRound(prompt("Enter your Choice = ").toLowerCase())
-  }
-}
-
-Game()
 
 function winner() {
-  let computerLost = userScore - computerScore
-  let userLost = computerScore - userScore
+  console.log(count);
+  let cS = document.getElementById("computerS")
+  let uS = document.getElementById("userS")
+  let dr = document.getElementById("draw")
+  let fS = document.getElementById("finalScore")
+  computerLost = userScore - computerScore
+  userLost = computerScore - userScore
   if(computerScore > userScore) {
-    console.log("Your Score = ", userScore)
-    console.log("Computer Score = ", computerScore)
-    console.log("Oops! You Lost the Game! You lost by ", userLost, " Points...")
+    fS.innerHTML = "<p>Computer Won by " + userLost + " Points!</p>"
   }
   else if(computerScore < userScore) {
-    console.log("Your Score = ", userScore)
-    console.log("Computer Score = ", computerScore)
-    console.log("Wohoo! You Win the Game! Computer lost by ", computerLost, " Points...")
+    fS.innerHTML = "<p>User Won by " + computerLost + " Points!</p>"
   }
   else {
-    console.log("Your Score = ", userScore)
-    console.log("Computer Score = ", computerScore)
-    console.log("The Game was a Draw.. Play Again! ")
-    Game()
+    fS.innerHTML = "<p>It's a Draw... Click on 'Play Again' button</p>"
+    resetGame()
   }
 }
 
-winner()
+
+function resetGame() {
+  let cS = document.getElementById("computerS")
+  let uS = document.getElementById("userS")
+  let dr = document.getElementById("draw")
+  let wins = document.getElementById("wins")
+  let cR = document.getElementById("currentRound")
+  let fS = document.getElementById("finalScore")
+  computerScore =0;
+  userScore = 0;
+  text = ''
+  cS.innerHTML = ""
+  uS.innerHTML = ""
+  dr.innerHTML = ""
+  wins.innerHTML = ""
+  cR.innerHTML = ""
+  fS.innerHTML = ""
+}
